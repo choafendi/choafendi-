d3ecb96e3b93aed0d14493ade3982b4ab0498934
choafendi <fendilouie1@gmail.com>
2017-06-21 15:49:00 -0400

    bill

---
+++ bill.rb
@@ -0,0 +1,72 @@
+# # storing your data S
+# bill = 40
+# num_people = 3
+# tip_percent = 0.20
+#
+# tip = bill * tip_percent
+# total = bill + tip
+# my_share = total / num_people
+# puts my_share
+
+age = 12
+
+
+name = "cynthia"
+puts "_I'm #{age + 8} years old and my mother is #{30}
+puts "#{name} #{age} is a great age to be, i love being #{age}
+
+
+#
+# cohort = "MIA C15"
+# num_students = 23
+#
+# lucky_number = [4,5,34,21]
+#
+# kyle = {name: "kyle", age 30 , gender: "male"}
+#
+# cohort_15_students = [cohort, num_students]
+#
+# p cohort_15_students
+#
+# students = [kyle]
+# p students
+# p "This cohort" + cohort + " has " + num_students.to_s  + "students"
+
+
+
+
+# arr = [1,2,3,]
+#  p arr
+#
+#  arr.delete(3)
+#
+#  p arr
+
+
+# str = "ACME supplies"
+# p str
+# p str.downcase
+# p str
+
+
+# p str
+# p str.downcase!
+# p str
+
+
+
+#
+# odd_number = [1,3,5,7]
+#
+# even_number = [2,4,6,8]
+#
+# number = []
+#
+# number = number + odd_number + even_number
+# p number
+
+
+
+p Math.sqrt (36)
+Math::PI
+p Math::PI
